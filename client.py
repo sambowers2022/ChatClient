e892022c4291ab97ea4fece2478b418c3ddd7e9e
sambowers2022 <bluebuilder99@gmail.com>
2024-05-31 17:29:15 -0500

    First working multi-client setup

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import threading
 from login import login
 HOST = "127.0.0.1"  # The server's hostname or IP address
 PORT = 65432  # The port used by the server
@@ -10,5 +11,15 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     s.sendall(str.encode(auth))
     code = int.from_bytes(s.recv(3))
     print(f"Received {code}")
-    if code==400:
-        token = s.recv(1024)
+    if code==200 or code==201:
+
+        def read_messages():
+            while True:
+                print(s.recv(1024).decode())
+        thread = threading.Thread(target=read_messages)
+        thread.start()
+
+        while True:
+            msg = input("Enter a message: ")
+            s.sendall(str.encode(msg))
+           
