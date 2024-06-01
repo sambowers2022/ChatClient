1a2c69605aa600ef7d1f8ab479be29dcc7152bd9
sambowers2022 <bluebuilder99@gmail.com>
2024-05-31 21:23:23 -0500

    Refactored login to be part of cli_client

--- cli_client.py
+++ cli_client.py
@@ -1,12 +1,37 @@
+from getpass import getpass
 import sys
-from login import login
 from client import SocketClient
+
+def login(client):
+    opt = input("Choose An Option ([L]ogin or [R]egister): ").lower()[0]
+    if opt=="l":
+        print("Login")
+    elif opt=="r":
+        print("Register")
+    else:
+        print("Invalid Option")
+        login()
+    
+    user, pwd = get_credentials()
+    if opt=="r":
+        confirm_password(pwd)
+    return client.auth(opt,user,pwd)
+
+def get_credentials():
+    usr = input("Username: ")
+    pwd = getpass("Password: ")
+    return usr, pwd
+
+def confirm_password(pwd):
+    if pwd!=getpass("Confirm password: "):
+        print("Passwords do not match")
+        confirm_password(pwd)
 
 host = len(sys.argv) > 1 and sys.argv[1] or "127.0.0.1"
 port = len(sys.argv) > 2 and int(sys.argv[2]) or 65432
 
 client = SocketClient(host, port)
-code = client.auth(login())
+code = login(client)
 if code == 200 or code == 201:
     client.read()
     try:
--- client.py
+++ client.py
@@ -1,4 +1,5 @@
 import socket
+import hashlib
 import multiprocessing
 
 class SocketClient:
@@ -9,9 +10,10 @@ class SocketClient:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect((self.host, self.port))
 
-    def auth(self, auth):
+    def auth(self, opt, usr, pwd):
         if self.socket is not None:
-            self.socket.sendall(str.encode(auth))
+            msg = f"{opt},{usr},{hashlib.sha256(pwd.encode()).hexdigest()}"
+            self.socket.sendall(str.encode(msg))
             code = int.from_bytes(self.socket.recv(3))
             return code
 
